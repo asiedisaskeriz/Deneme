16806ae0505591dfa6afcfed4bf62794822a9c93
asiedisaskeriz <68537410+asiedisaskeriz@users.noreply.github.com>
2024-03-27 18:21:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ import requests
 import websocket
 from keep_alive import keep_alive
 
-status = "online" 
+status = "dnd" #online/dnd/idle
 
-GUILD_ID = "1168551939299086386" 
-CHANNEL_ID = "1170013140193390632"
+GUILD_ID = 1168551939299086386
+CHANNEL_ID = 1170013140193390632
 SELF_MUTE = False
 SELF_DEAF = False
 
-token = os.environ.get("TOKEN")
-if not token:
+usertoken = os.getenv("TOKEN")
+if not usertoken:
   print("[ERROR] Please add a token inside Secrets.")
   sys.exit()
 
-headers = {"Authorization": token, "Content-Type": "application/json"}
+headers = {"Authorization": usertoken, "Content-Type": "application/json"}
 
 validate = requests.get('https://canary.discordapp.com/api/v9/users/@me', headers=headers)
 if validate.status_code != 200:
@@ -35,21 +35,18 @@ def joiner(token, status):
     ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
     start = json.loads(ws.recv())
     heartbeat = start['d']['heartbeat_interval']
-    
     auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
-    
     vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
-    ws.send(json.dumps(vc)) 
-    
     ws.send(json.dumps(auth))
+    ws.send(json.dumps(vc))
     time.sleep(heartbeat / 1000)
     ws.send(json.dumps({"op": 1,"d": None}))
 
 def run_joiner():
   os.system("clear")
-  print("Logged in as {}#{} ({}).".format(username, discriminator, userid))
+  print(f"Logged in as {username}#{discriminator} ({userid}).")
   while True:
-    joiner(token, status)
+    joiner(usertoken, status)
     time.sleep(30)
 
 keep_alive()
