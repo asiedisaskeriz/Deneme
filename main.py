1541d09c16df7680e8ef62d1d94a9d00bfa31044
asiedisaskeriz <68537410+asiedisaskeriz@users.noreply.github.com>
2024-03-27 18:42:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -34,15 +34,22 @@ userid = userinfo["id"]
 def joiner(token, status):
 
   ws = websocket_client.WebSocket()
-    ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
-    start = json.loads(ws.recv())
-    heartbeat = start['d']['heartbeat_interval']
-    auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
-    vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
-    ws.send(json.dumps(auth))
-    ws.send(json.dumps(vc))
-    time.sleep(heartbeat / 1000)
-    ws.send(json.dumps({"op": 1,"d": None}))
+
+  ws.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+  
+  start = json.loads(ws.recv())
+  heartbeat = start['d']['heartbeat_interval']
+
+  auth = {"op": 2,"d": {"token": token,"properties": {"$os": "Windows 10","$browser": "Google Chrome","$device": "Windows"},"presence": {"status": status,"afk": False}},"s": None,"t": None}
+  
+  vc = {"op": 4,"d": {"guild_id": GUILD_ID,"channel_id": CHANNEL_ID,"self_mute": SELF_MUTE,"self_deaf": SELF_DEAF}}
+
+  ws.send(json.dumps(auth))
+  ws.send(json.dumps(vc))
+
+  time.sleep(heartbeat / 1000)
+
+  ws.send(json.dumps({"op": 1,"d": None}))
 
 def run_joiner():
   os.system("clear")
